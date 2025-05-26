2e35e9fc55608deb45396eab0c179c792f3e8a90
Herdiansyh <mohammadherdiansyah84@gmail.com>
2025-05-26 10:26:56 +0700

    configuration setup

--- resources/js/lib/utils.js
+++ resources/js/lib/utils.js
@@ -1,6 +1,21 @@
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge"
+import { clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs) {
-  return twMerge(clsx(inputs));
+    return twMerge(clsx(inputs));
 }
+
+export const STATUS = {
+    TODO: 'To Do',
+    IN_PROGRESS: 'In Progress',
+    ONREVIEW: 'On Review',
+    DONE: 'Done',
+    UNKNOW: 'Unknown',
+};
+export const PRIORITY = {
+    LOW: 'Low',
+    MEDIUM: 'Medium',
+    HIGH: 'High',
+    URGENT: 'Urgent',
+    UNKNOW: 'Unknown',
+};
