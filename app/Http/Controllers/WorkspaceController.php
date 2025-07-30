660807681e6025f7e8e610bd63f6e3698470617d
Herdiansyh <mohammadherdiansyah84@gmail.com>
2025-07-30 11:31:03 +0700

    show workspaces

--- app/Http/Controllers/WorkspaceController.php
+++ app/Http/Controllers/WorkspaceController.php
@@ -4,9 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Enums\WorkspaceVisibility;
 use App\Http\Requests\WorkSpaceRequest;
+use App\Http\Resources\WorkspaceResource;
 use App\Models\Workspace;
 use App\Traits\HasFile;
+use Inertia\Response;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Inertia\Response as InertiaResponse;
 
 class WorkspaceController extends Controller
 {
@@ -24,9 +28,9 @@ class WorkspaceController extends Controller
             'visibilities' => WorkspaceVisibility::options(),
         ]);
     }
-    public function store(WorkSpaceRequest $request)
+    public function store(WorkSpaceRequest $request) :RedirectResponse
     {
-$request->user()->workspaces()->create([
+        $workspace =$request->user()->workspaces()->create([
             'name' => $name = $request->name,
             'slug' => str()->slug($name . str()->uuid(10)),
             'cover' => $this->upload_file($request, 'cover', 'workspaces/cover'), 
@@ -35,7 +39,14 @@ $request->user()->workspaces()->create([
         ]);
 
         flashMessage('Workspace created successfully','success');
- return back();
+ return to_route('workspaces.show', $workspace);
     }
 
+    public function show (Workspace $workspace):Response
+    {
+        return inertia(component : 'Workspaces/Show', props: [
+            'workspace' => fn()=> new WorkspaceResource($workspace),
+         
+        ])
+    ;}
 }
