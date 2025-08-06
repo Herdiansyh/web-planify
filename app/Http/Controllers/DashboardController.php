6b8b4a99de9a80c7bb03a9a95308332a657697f7
Herdiansyh <mohammadherdiansyah84@gmail.com>
2025-08-06 12:09:52 +0700

    statisik dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,14 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\CardStatus;
+use App\Models\Card;
+use App\Models\Member;
+use App\Models\User;
+use App\Models\Workspace;
 use Illuminate\Http\Request;
 use Inertia\Response;
 
 class DashboardController extends Controller
 {
     //
-    public function index(): Response
-    {
-        return inertia('Dashboard');
-    }
+   public function index(): Response
+{
+    return inertia('Dashboard', [
+        'page_settings' => [
+            'title' => 'dashboard',
+            'subtitle' => 'You can see a summary of the information here',
+        ],
+        'count' => [
+            'users' => User::count(),
+            'workspace' => Member::query()
+                ->where('members.user_id', request()->user()->id)
+                ->whereHasMorph('memberable', Workspace::class)
+                ->count(),
+            'task' => Member::query()
+                ->where('members.user_id', request()->user()->id)
+                ->whereHasMorph('memberable', Card::class)
+                ->count(),
+            'dones' => Member::query()
+                ->where('members.user_id', request()->user()->id)
+                ->whereHasMorph('memberable', Card::class, fn($query) =>
+                    $query->where('status', CardStatus::DONE->value)
+                )
+                ->count(),
+        ]
+    ]);
+}
+
 }
