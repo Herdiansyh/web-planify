aa3efef626e49b964cb262baa5f1e0cda54f588d
Herdiansyh <mohammadherdiansyah84@gmail.com>
2025-08-07 12:44:22 +0700

    to do list dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Enums\CardStatus;
+use App\Http\Resources\MyTaskResource;
 use App\Models\Card;
 use App\Models\Member;
 use App\Models\User;
@@ -16,11 +17,23 @@ class DashboardController extends Controller
     //
    public function index(): Response
 {
+    $tasks = Member::query()
+    ->where('members.user_id', request()->user()->id)
+    ->whereHasMorph(
+        'memberable',
+        Card::class,
+        fn($query)=> $query->where('status', CardStatus::TODO->value)
+    )
+    ->latest()
+    ->limit(10)
+    ->get();
+
     return inertia('Dashboard', [
         'page_settings' => [
             'title' => 'dashboard',
             'subtitle' => 'You can see a summary of the information here',
         ],
+        'tasks' => MyTaskResource::collection($tasks),
         'productivity_chart' => $this->productivityChart(),
         'count' => [
             'users' => User::count(),
